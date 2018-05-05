82cca160674f13ef577e50fb76f9fcf090af78b5
SteveAnda <37079180+SteveAnda@users.noreply.github.com>
2018-05-05 09:33:31 +0800

    Update install_sougou.bash

--- install_sougou.bash
+++ install_sougou.bash
@@ -5,3 +5,18 @@ sudo apt-get update
 sudo apt-get install sogoupinyin 
 echo "sogoupinyin has been installed in your computer"
 echo "if you can't find it,you need to restart your computer"
+
+#以下不知有没有用（第一次安装时用到了），可以先试下上面步骤（fcitx可以直接从图形化界面下载）
+# before:
+#①先添加以下源
+sudo add-apt-repository ppa:fcitx-team/nightly
+#②添加源之后需要更新一下系统
+sudo apt-get update
+#③开始安装fcitx
+sudo apt-get install fcitx
+#④接着安装fcitx的配置工具
+sudo apt-get install fcitx-config-gtk
+#⑤然后安装fcitx的table-all软件包
+sudo apt-get install fcitx-table-all
+#⑥安装im-switch切换工具（此步导致系统设置语言支持栏消失，可不执行）
+sudo apt-get install im-switch
